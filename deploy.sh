73863b01a7333bf9d070dacab68c212cd239c093
Chalermrid Patipanang <chalermrid.patipanang@lotusretails.com>
2021-04-16 22:44:57 +0700

    remove artifact folder after deployment has been finished

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ mkdir -p artifact
 sam build
 sam package --template-file template.yaml --s3-bucket luckalot-scheduler-artifact --output-template-file artifact/template.${StageName:-"dev"}.out.yaml --profile luckalot.${StageName:-"dev"}
 sam deploy --template-file artifact/template.${StageName:-"dev"}.out.yaml --s3-bucket luckalot-scheduler-artifact --stack-name luckalot-scheduler-stack --capabilities CAPABILITY_NAMED_IAM --profile luckalot.${StageName:-"dev"}
+
+rm -rf artifact
